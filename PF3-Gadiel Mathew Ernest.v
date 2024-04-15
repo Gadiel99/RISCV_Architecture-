a04fd3f9f789935d991714a0d9b4ff3b4c6cac64
Gadiel99 <gadiel.dejesus@upr.edu>
2024-04-14 23:41:33 -0400

    -changed the S name in display

--- PF3-Gadiel Mathew Ernest.v
+++ PF3-Gadiel Mathew Ernest.v
@@ -69,7 +69,7 @@ module instruction_memory(
     //Reading the preload memory
     //If this is not working specified the whole directory of the file.
     initial begin
-      $readmemb("C:/test-code.txt", mem, 0, 511);
+      $readmemb("C:/Users/jay20/Documents/RISCV_Architecture-/test-code.txt", mem, 0, 511);
     end 
     
     //Making the arragment for the instruction
@@ -929,9 +929,11 @@ always #1 clk = !clk;
     //              uut.EX_MEM_pipeline_register_inst.mem_mem_ins_enable, uut.EX_MEM_pipeline_register_inst.mem_mem_write,
     //              uut.EX_MEM_pipeline_register_inst.mem_size,
     //              uut.MEM_WB_pipeline_register_inst.wb_rf_enable);
-        //$display("test");
-        $display("\nTime: %t \nPC:%d \nInstruction Fetched: %b", $time,uut.pc_reg_inst.out, uut.instruction_memory_inst.instruction);
 
+        $display("\nTime: %t \nPC:%d \nInstruction Fetched: %b", $time,uut.pc_reg_inst.out, uut.instruction_memory_inst.instruction);
+        
+        $display("\nS: %b", s);
+        
         $display("\n| ID Signals: RF En %b, ALU Op %b, SOH %b, Load Inst %b, Mem Ins En %b, MemWrite %b, Size %b, SE %b Full Cond %b, JALR Sig %b, AUIPC S %b, JAL Sig %b |", 
                 uut.control_unit_inst.id_rf_enable, uut.control_unit_inst.id_alu_op, uut.control_unit_inst.id_shifter_imm, uut.control_unit_inst.id_load_inst,
                 uut.control_unit_inst.id_mem_ins_enable, uut.control_unit_inst.id_mem_write, uut.control_unit_inst.size, uut.control_unit_inst.id_se,
@@ -960,7 +962,7 @@ always #1 clk = !clk;
         
         );
 
-        $display("\n CU S: %b", s);
+        
 
     end
 end
