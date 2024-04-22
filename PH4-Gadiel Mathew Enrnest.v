39fd73e0026624bf6f248e9f3e7e0136fafb4c6d
Gadiel99 <gadiel.dejesus@upr.edu>
2024-04-21 21:40:22 -0400

    -fixed semicolons and added signals to in EX/WB instance.

--- PH4-Gadiel Mathew Enrnest.v
+++ PH4-Gadiel Mathew Enrnest.v
@@ -514,12 +514,12 @@ module MEM_WB_pipeline_register(
         
         if(reset == 1) begin
             $display("-------------NOP MEM/WB--------------");
-            mem_mux2x1_mem_output <= 1'b0;
+            wb_mem_mux2x1_mem_output  <= 1'b0;
             wb_rf_enable <= 1'b0;
 
         end else begin
         //Control Unit signals  
-            wb_mem_mux2x1_mem_output <= mem_mux2x1_mem_output
+            wb_mem_mux2x1_mem_output <= mem_mux2x1_mem_output;
             wb_rf_enable <= mem_rf_enable;
         end
     end
@@ -1151,27 +1151,23 @@ module processor(
 
     //Signal Selector Muxes
     mux2x1 mux2x1_data_memory(
-        .input0(mem_PB),
+        .input0(mem_mux2x1_alu_output_output),
         .input1(mem_out),
         .control_signal(mem_load_inst),
         .output_value(mux2x1_mem_output)
     );
 
     // MEM/WB Pipeline Register
-    
-   
-    
     MEM_WB_pipeline_register MEM_WB_pipeline_register_inst(
         .clk(clk),
         .reset(reset),
         .s(s),
+        .mem_mux2x1_mem_output(mem_mux2x1_mem_output),
         .mem_rf_enable(mem_rf_enable),
-        .wb_rf_enable(wb_rf_enable)
+        .wb_rf_enable(wb_rf_enable),
+        .wb_mux2x1_mem_output(wb_mux2x1_mem_output)
     );
 
-    
-    
-    
     
     // Control Unit
     control_unit control_unit_inst(
@@ -1257,13 +1253,5 @@ module processor(
         .output_value(ex_mux2x1_alu_output_output)
     );
 
-    
-
-    
-
-    
-
-    // Next PC Logic (Placeholder for actual logic)
-    //assign pc_next = pc_current + 4;
 
 endmodule
