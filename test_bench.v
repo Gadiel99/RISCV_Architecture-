163c05992b11210c69b430526e134028453d0586
Gadiel99 <gadiel.dejesus@upr.edu>
2024-05-08 22:46:37 -0400

    - Commented some testing code

--- test_bench.v
+++ test_bench.v
@@ -25,31 +25,30 @@ module processor_tb;
 
     // Inicialización y simulación
     initial begin
-        #10 reset = 0; // Desactiva el reset
+        #3 reset = 0; // Desactiva el reset
         
-        #300$finish; // Termina la simulación
+        #50$finish; // Termina la simulación
     end
     
    //Monitoreo de senales
    always @(posedge clk) begin
-        // $monitor("Time = %t, PC = %d, Instruction = %b, ALU_Out = %h, ID_PA = %d, ID_PB = %d, ID_RD = %d, WB_Output = %d, WB_Rf_Enable = %d",
-        //          $time,
-        //          uut.pc_current,
-        //          uut.instruction,
-        //          uut.ALU_inst.Out,
-        //          uut.registerfile_inst.PA,
-        //          uut.registerfile_inst.PB,
-        //          uut.MEM_WB_pipeline_register_inst.wb_rd,
-        //          uut.MEM_WB_pipeline_register_inst.wb_mux2x1_mem_output,
-        //          uut.MEM_WB_pipeline_register_inst.wb_rf_enable
-        //         );
+        $monitor("\n\tTime = %t, \n\tPC = %d, \n\tInstruction = %b, \n\tr1 = %d, \n\tr2 = %d, \n\tr3 = %d, \n\tr5 = %d, \n\tr6 = %d",
+                 $time,
+                 uut.pc_current,
+                 uut.instruction,
+                 uut.registerfile_inst.registers1,
+                 uut.registerfile_inst.registers2,
+                 uut.registerfile_inst.registers3,
+                 uut.registerfile_inst.registers5,
+                 uut.registerfile_inst.registers6,
+                );
 
-        $display("\nTime = %t, \nPC = %d, \nInstruction = %b, \nreset = %b",
-            $time,
-            uut.pc_current,
-            uut.instruction,
-            uut.reset
-        );
+        // $display("\nTime = %t, \nPC = %d, \nInstruction = %b, \nreset = %b",
+        //     $time,
+        //     uut.pc_current,
+        //     uut.IF_ID_pipeline_register_inst.instruction,
+        //     uut.reset
+        // );
         
         // $display("\nAdder PC");
         // $monitor("\tpc = %d,\n\tpc_next = %d",
@@ -57,7 +56,7 @@ module processor_tb;
         //         uut.pc_adder.pcplus4
         // );
         
-        //IF/ID
+        
         // $display("\nIF/ID pipeline");
         // $monitor("\treset =%b, \tLE = %b, \n\tins_mem_out = %b,\n\tPC = %d,\n\tinstruction = %b,\n\tid_pc = %d,\n\tid_imm12_S = %b, \n\tid_imm12_I = %b,\n\tid_rn = %b, id_rm = %b,\n\tid_imm20 = %b,\n\tid_rd = %b,\n\tid_pc_next = %d",
         //          uut.IF_ID_pipeline_register_inst.reset,
@@ -75,46 +74,52 @@ module processor_tb;
         //          uut.IF_ID_pipeline_register_inst.id_pc_next        
         // );
 
-        $display("\nID/EX pipeline");
-        $monitor("\treset = %b,\n\tid_alu_op_mux = %b,\n\tid_shifter_imm_mux = %b,\n\tid_rf_enable_mux = %b,\n\tid_load_inst_mux = %b,\n\tid_mem_ins_enable_mux = %b,\n\tid_mem_write_mux = %b,\n\tsize_mux = %b,\n\tid_se_mux = %b,\n\tid_full_cond_mux = %b,\n\tid_TA = %d,\n\tid_pc = %d,\n\tid_PA = %d,\n\tid_PB = %d,\n\tid_imm12_I = %b,\n\tid_imm12_S = %b,\n\tid_pc_next = %d,\n\tid_imm20 = %b,\n\tid_rd = %d,\n\tex_rf_enable = %b,\n\tex_alu_op= %b,\n\tex_shifter_imm = %b,\n\tex_load_inst = %b,\n\tex_mem_ins_enable = %b,\n\tex_mem_write = %b,\n\tex_size= %b,\n\tex_se = %b,\n\tex_full_cond = %b,\n\tex_TA = %d,\n\tex_pc = %d,\n\tex_PA = %d,\n\tex_PB = %d,\n\tex_imm12_I = %b,\n\tex_imm12_S = %b,\n\tex_pc_next = %d,\n\tex_rd = %d",
-        uut.ID_EX_pipeline_register_inst.reset,
-        uut.ID_EX_pipeline_register_inst.id_alu_op_mux,
-        uut.ID_EX_pipeline_register_inst.id_shifter_imm_mux,
-        uut.ID_EX_pipeline_register_inst.id_rf_enable_mux,
-        uut.ID_EX_pipeline_register_inst.id_load_inst_mux,
-        uut.ID_EX_pipeline_register_inst.id_mem_ins_enable_mux,
-        uut.ID_EX_pipeline_register_inst.id_mem_write_mux,
-        uut.ID_EX_pipeline_register_inst.size_mux,
-        uut.ID_EX_pipeline_register_inst.id_se_mux,
-        uut.ID_EX_pipeline_register_inst.id_full_cond_mux,
-        uut.ID_EX_pipeline_register_inst.id_TA,
-        uut.ID_EX_pipeline_register_inst.id_pc,
-        uut.ID_EX_pipeline_register_inst.id_PA,
-        uut.ID_EX_pipeline_register_inst.id_PB,
-        uut.ID_EX_pipeline_register_inst.id_imm12_I,
-        uut.ID_EX_pipeline_register_inst.id_imm12_S,
-        uut.ID_EX_pipeline_register_inst.id_pc_next,
-        uut.ID_EX_pipeline_register_inst.id_imm20,
-        uut.ID_EX_pipeline_register_inst.id_rd,
-        uut.ID_EX_pipeline_register_inst.ex_rf_enable,
-        uut.ID_EX_pipeline_register_inst.ex_alu_op,
-        uut.ID_EX_pipeline_register_inst.ex_shifter_imm,
-        uut.ID_EX_pipeline_register_inst.ex_load_inst,
-        uut.ID_EX_pipeline_register_inst.ex_mem_ins_enable,
-        uut.ID_EX_pipeline_register_inst.ex_mem_write,
-        uut.ID_EX_pipeline_register_inst.ex_size,
-        uut.ID_EX_pipeline_register_inst.ex_se,
-        uut.ID_EX_pipeline_register_inst.ex_full_cond,
-        uut.ID_EX_pipeline_register_inst.ex_TA,
-        uut.ID_EX_pipeline_register_inst.ex_pc,
-        uut.ID_EX_pipeline_register_inst.ex_PA,
-        uut.ID_EX_pipeline_register_inst.ex_PB,
-        uut.ID_EX_pipeline_register_inst.ex_imm12_I,
-        uut.ID_EX_pipeline_register_inst.ex_imm12_S,
-        uut.ID_EX_pipeline_register_inst.ex_pc_next,
-        uut.ID_EX_pipeline_register_inst.ex_imm20,
-        uut.ID_EX_pipeline_register_inst.ex_rd
-        );
+        // $display("\nID/EX pipeline");
+        // $monitor("\treset = %b,\n\tid_alu_op_mux = %b,\n\tid_shifter_imm_mux = %b,\n\tid_rf_enable_mux = %b,\n\tid_load_inst_mux = %b,\n\tid_mem_ins_enable_mux = %b,\n\tid_mem_write_mux = %b,\n\tsize_mux = %b,\n\tid_se_mux = %b,\n\tid_full_cond_mux = %b,\n\tid_jalr_sig_mux = %b,\n\tid_auipc_s_mux= %b,\n\tid_jal_sig_mux = %b, \n\tid_TA = %d,\n\tid_pc = %d,\n\tid_PA = %d,\n\tid_PB = %d,\n\tid_imm12_I = %b,\n\tid_imm12_S = %b,\n\tid_pc_next = %d,\n\tid_imm20 = %b,\n\tid_rd = %d,\n\tex_rf_enable = %b,\n\tex_alu_op= %b,\n\tex_shifter_imm = %b,\n\tex_load_inst = %b,\n\tex_mem_ins_enable = %b,\n\tex_mem_write = %b,\n\tex_size= %b,\n\tex_se = %b,\n\tex_full_cond = %b,\n\tex_jalr_sig = %b,\n\tex_auipc_s = %b,\n\tex_jal_sig = %b,\n\tex_TA = %d,\n\tex_pc = %b,\n\tex_PA = %d,\n\tex_PB = %d,\n\tex_imm12_I = %b,\n\tex_imm12_S = %b,\n\tex_pc_next = %d,\n\tex_imm20 = %b\n\tex_rd = %d",
+        // uut.ID_EX_pipeline_register_inst.reset,
+        // uut.ID_EX_pipeline_register_inst.id_alu_op_mux,
+        // uut.ID_EX_pipeline_register_inst.id_shifter_imm_mux,
+        // uut.ID_EX_pipeline_register_inst.id_rf_enable_mux,
+        // uut.ID_EX_pipeline_register_inst.id_load_inst_mux,
+        // uut.ID_EX_pipeline_register_inst.id_mem_ins_enable_mux,
+        // uut.ID_EX_pipeline_register_inst.id_mem_write_mux,
+        // uut.ID_EX_pipeline_register_inst.size_mux,
+        // uut.ID_EX_pipeline_register_inst.id_se_mux,
+        // uut.ID_EX_pipeline_register_inst.id_full_cond_mux,
+        // uut.ID_EX_pipeline_register_inst.id_jalr_sig_mux,
+        // uut.ID_EX_pipeline_register_inst.id_auipc_s_mux,
+        // uut.ID_EX_pipeline_register_inst.id_jal_sig_mux,
+        // uut.ID_EX_pipeline_register_inst.id_TA,
+        // uut.ID_EX_pipeline_register_inst.id_pc,
+        // uut.ID_EX_pipeline_register_inst.id_PA,
+        // uut.ID_EX_pipeline_register_inst.id_PB,
+        // uut.ID_EX_pipeline_register_inst.id_imm12_I,
+        // uut.ID_EX_pipeline_register_inst.id_imm12_S,
+        // uut.ID_EX_pipeline_register_inst.id_pc_next,
+        // uut.ID_EX_pipeline_register_inst.id_imm20,
+        // uut.ID_EX_pipeline_register_inst.id_rd,
+        // uut.ID_EX_pipeline_register_inst.ex_rf_enable,
+        // uut.ID_EX_pipeline_register_inst.ex_alu_op,
+        // uut.ID_EX_pipeline_register_inst.ex_shifter_imm,
+        // uut.ID_EX_pipeline_register_inst.ex_load_inst,
+        // uut.ID_EX_pipeline_register_inst.ex_mem_ins_enable,
+        // uut.ID_EX_pipeline_register_inst.ex_mem_write,
+        // uut.ID_EX_pipeline_register_inst.ex_size,
+        // uut.ID_EX_pipeline_register_inst.ex_se,
+        // uut.ID_EX_pipeline_register_inst.ex_full_cond,
+        // uut.ID_EX_pipeline_register_inst.ex_jalr_sig,
+        // uut.ID_EX_pipeline_register_inst.ex_auipc_s,
+        // uut.ID_EX_pipeline_register_inst.ex_jal_sig,
+        // uut.ID_EX_pipeline_register_inst.ex_TA,
+        // uut.ID_EX_pipeline_register_inst.ex_pc,
+        // uut.ID_EX_pipeline_register_inst.ex_PA,
+        // uut.ID_EX_pipeline_register_inst.ex_PB,
+        // uut.ID_EX_pipeline_register_inst.ex_imm12_I,
+        // uut.ID_EX_pipeline_register_inst.ex_imm12_S,
+        // uut.ID_EX_pipeline_register_inst.ex_pc_next,
+        // uut.ID_EX_pipeline_register_inst.ex_imm20,
+        // uut.ID_EX_pipeline_register_inst.ex_rd
+        // );
 
 
 
@@ -146,12 +151,24 @@ module processor_tb;
         //          uut.hazard_forwarding_unit_inst.pc_enable  
         // );
 
-        // $display("\nProgram counter");
-        // $monitor("\treset = %b, \n\tenable = %b, \n\tin = %d, \n\tout = %d",
-        //           uut.pc_reg_inst.reset,
+        // $display("\nCondition handler");
+        // $monitor("\tcontrol_hazard_out = %b, \n\tZ flag = %b, \n\tN flag = %b,\n\tex_full_cond = %b",
+        //         uut.condition_handler_inst.control_hazard_out,
+        //         uut.condition_handler_inst.Z_flag,
+        //         uut.condition_handler_inst.N_flag,
+        //         uut.condition_handler_inst.ex_full_cond
+        // );
+
+        // $display("\nProgram counter & IF_TA_MUX");
+        // $monitor("\n\tenable = %b,\n\tpc = %d, \n\tadder_input = %d,\n\tadder_output = %d,\n\tinput1_mux = %d,\n\tcontrol_signal_mux = %b, \n\toutput_value_mux = %d",
+                  
         //           uut.pc_reg_inst.en,
-        //           uut.pc_reg_inst.in,
-        //           uut.pc_reg_inst.out  
+        //           uut.pc_reg_inst.out,
+        //           uut.pc_current,
+        //           uut.pc_next,
+        //           uut.mux2x1_if_TA.input1,
+        //           uut.mux2x1_if_TA.control_signal,
+        //           uut.mux2x1_if_TA.output_value 
         // );
 
         // $display("\nif_TA MUX");
@@ -163,11 +180,15 @@ module processor_tb;
         // );
 
         // $display("\nid_TA MUX");
-        // $monitor("\tinput0 = %d,\n\tinput1 = %d,\n\tcontrol_signal = %b, \n\toutput_value = %d",
+        // $monitor("\tpc = %d,\n\tinput0 = %d,\n\tinput1 = %d,\n\tcontrol_signal = %b, \n\toutput_value = %d, \n\tid_jal_sig = %b, \n\tex_jalr_sig = %b,\n\tcontrol_hazard_signal = %b",
+        //         uut.pc_current,
         //         uut.mux2x1_id_TA.input0,
         //         uut.mux2x1_id_TA.input1,
         //         uut.mux2x1_id_TA.control_signal,
-        //         uut.mux2x1_id_TA.output_value
+        //         uut.mux2x1_id_TA.output_value,
+        //         uut.id_jal_sig,
+        //         uut.ex_jalr_sig,
+        //         uut.control_hazard_signal
         // );
 
         // $display("\nid_adder_input MUX");
